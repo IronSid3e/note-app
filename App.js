24e22953d8525cde48c7aa19ddce1912a695bf33
IronSid3e <omertahirustun05@hotmail.com>
2026-02-14 12:02:51 +0300

    set navigation and screens

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStaticNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import CreateScreen from "./screens/CreateScreen";
 
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+return (
+  <Stack.Navigator screenOptions={{headerTitle: 'Note App'}}>
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="Create" component={CreateScreen} />
+  </Stack.Navigator>
+);
+}
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack/>
+    </NavigationContainer>
   );
 }
 
